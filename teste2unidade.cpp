0b672fca4b3c6dcab1c506529af10181cf3e69b4
juliamiranda <51089775+juliamiranda@users.noreply.github.com>
2019-06-09 20:15:18 -0300

    Add files via upload

---
+++ teste2unidade.cpp
@@ -0,0 +1,28 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "TESTE2.cpp"
+#include <doctest.h>
+
+
+
+class Teste {
+public:
+    static string abrir_arquivos (Armazenar_arquivos &nomearquivos){
+        /*for( int x = 0; x <= nomearquivos.nomearquivos.size(); x++ ){
+        std::ifstream f;
+        std::string name = "./20news-19997//20_newsgroups//" + nomearquivos.nomearquivos[x];
+        f.open( name.c_str(), std::ios::in );
+        string resultado;
+        if
+
+    }*/
+        return teste.copiar_arquivo;}
+
+        static string conferir_maiuscula
+
+TEST_SUITE ("Index"){
+    TEST_CASE("copiar_arquivo"){
+        teste.copiar_arquivo;
+        CHECK(Teste::abrir_arquivos()_arquivo!=NULL}
+}
+    };
+
